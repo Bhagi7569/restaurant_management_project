764a73f503cf286170f99599673d5dabe55e528a
Bhagi7569 <bsarukolla@gmail.com>
2025-08-10 08:41:52 +0530

    26888

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
+from django.shortcuts import render
 
-# Register your models here.
+def restaurant_page(request):
+    restaurant_info = {
+            'description': 'A cozy place serving delicious food.',
+                    'image_url': '/static/images/restaurant.jpg'
+                        }
+                            return render(request, 'restaurant.html', {'restaurant': restaurant_info})
+                            django.urls
