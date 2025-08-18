dd571df1bdc0023c523ff5e47522e1075c47437b
Bhagi7569 <bsarukolla@gmail.com>
2025-08-18 22:21:05 +0530

    33872

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,34 @@
 from django.urls import path
 from .views import *
 
+jango.urls import path
+from .views import *
+
+urlpatterns = ango.urls import path
+from .views import *
+
+[rt path
+from .views import *
+
+urlpatterns = ango.urls import path
+from .views import *
+
+ws import *
+
+rls import path
+from .views import *
+
 urlpatterns = [
-    
-]
+  mport *
+
+urlpatterns = ango.urls import path
+from .views import *
+
+ws import *
+
+ views import *
+
+ws import *
+
+ 
+ ]
