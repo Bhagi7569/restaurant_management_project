2240d83e4aa7db94651c5e0dd4f116bc972134e9
Bhagi7569 <bsarukolla@gmail.com>
2025-08-25 16:59:19 +0530

    38296

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+# views.py
+from django.shortcuts import render
+import requests
 
-# Create your models here.
+def homepage(request):
+    # Assuming an API endpoint for menu items
+        api_url = 'http://localhost:8000/api/menu_items/'  # Example API URL
+            try:
+                    response = requests.get(api_url)
+                            menu_items = response.json()
+                                except requests.RequestException:
+                                        menu_items = []
+                                            return render(request, 'homepage.html', {'menu_items': menu_items})
