2f16b733cbd25b2a089a7c5738d9561d5a5e9038
Bhagi7569 <bsarukolla@gmail.com>
2025-08-27 15:03:43 +0530

    38299

--- account/models.py
+++ account/models.py
@@ -1,12 +1,15 @@
-# admin.py
-from django.contrib import admin
-from .models import MenuItem, Order, OrderItem
+# views.py
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import MenuItemSerializer
 
-admin.site.register(MenuItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)# views.py
-from django.shortcuts import render
-import requests
-
-def homepage(request):
-    # Assuming an API endpoint for                                                                                        quantity = models.PositiveIntegerField(default=1)
+class MenuAPIView(APIView):
+    def get(self, request):
+            # Hardcoded menu data for simplicity
+                    menu_data = [
+                                {'name': 'Burger', 'description': 'Juicy beef burger', 'price': 8.99},
+                                            {'name': 'Pizza', 'description': 'Cheese pizza with veggies', 'price': 10.99},
+                                                        {'name': 'Salad', 'description': 'Fresh garden salad', 'price': 6.99},
+                                                                ]
+                                                                        serializer = MenuItemSerializer(menu_data, many=True)
+                                                                                return Response(serializer.data)                                                                                  quantity = models.PositiveIntegerField(default=1)
