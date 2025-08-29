22e2a8e09f3f45c46d4c2fc62226eee6e6e3b7c8
Bhagi7569 <bsarukolla@gmail.com>
2025-08-29 09:10:17 +0530

    40044

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,10 @@
-from django.urls import path
-from .views import *
+# models.py
+from django.db import models
 
-urlpatterns = [
-    
-]
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+        description = models.TextField()
+            price = models.DecimalField(max_digits=6, decimal_places=2)
+
+                def __str__(self):
+                        return self.name
