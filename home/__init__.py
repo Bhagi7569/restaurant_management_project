4d8e972a1b6558e2e3e14999c873fd6039cda2a9
Bhagi7569 <bsarukolla@gmail.com>
2025-08-29 08:51:55 +0530

    40042

--- home/__init__.py
+++ home/__init__.py
@@ -1,21 +1,12 @@
-     <!-- templates/about.html -->
+# models.py
+from django.db import models
 
-     <!DOCTYPE html>
-     <html lang="en">
-     <head>
-         <meta charset="UTF-8">
-             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                 <title>About {{ restaurant_name }}</title>
-                 </head>
-                 <body>
-                     <nav>
-                             <a href="{% url 'homepage' %}">Home</a> | About
-                                 </nav>
-                                     <h1>About {{ restaurant_name }}</h1>
-                                         <p>{{ restaurant_description }}</p>
-                                             {% if restaurant_image %}
-                                                     <img src="{{ restaurant_image }}" alt="Image of {{ restaurant_name }}">
-                                                         {% endif %}
-                                                         </body>
-                                                         </html>
-                                                                                                                                                                                 
+class ContactSubmission(models.Model):
+    name = models.CharField(max_length=100)
+        email = models.EmailField()
+            submitted_at = models.DateTimeField(auto_now_add=True)
+
+                def __str__(self):
+                        return f"Submission from {self.name}"
+        
+                                                                                         
