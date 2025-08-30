05ca4e5ebec112787cb474a18ec3ab35ab234353
Bhagi7569 <bsarukolla@gmail.com>
2025-08-30 08:45:14 +0530

    40521

--- account/tests.py
+++ account/tests.py
@@ -5,9 +5,5 @@ from django.shortcuts import render
 def homepage(request):
     restaurant_name = settings.RESTAURANT_NAME
         return render(request, 'homepage.html', {'restaurant_name': restaurant_name})
-        # models.py
-        from django.db import models
-
-        class Restaurant(models.Model):
-            name = models.CharField(max_length=100)
-                # other fields...
+    
+    
