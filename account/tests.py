1fc7fc20103b9c894e0de76a023cb02b87143807
Bhagi7569 <bsarukolla@gmail.com>
2025-08-30 08:48:12 +0530

    40522

--- account/tests.py
+++ account/tests.py
@@ -1,9 +1,13 @@
-# views.py
-from django.conf import settings
-from django.shortcuts import render
+# models.py
+from django.db import models
 
-def homepage(request):
-    restaurant_name = settings.RESTAURANT_NAME
-        return render(request, 'homepage.html', {'restaurant_name': restaurant_name})
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+        description = models.TextField()
+            price = models.DecimalField(max_digits=6, decimal_places=2)
+                image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+                
+                    def __str__(self):
+                            return self.name
     
     
