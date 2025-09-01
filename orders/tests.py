ebf11dabd59591acbb3f351b5b472d9106efbe55
Bhagi7569 <bsarukolla@gmail.com>
2025-09-01 08:53:04 +0530

    40525

--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+# models.py
+from django.db import models
 
-# Create your tests here.
+class RestaurantLocation(models.Model):
+    address = models.CharField(max_length=200)
+        city = models.CharField(max_length=100)
+            state = models.CharField(max_length=50)
+                zip_code = models.CharField(max_length=10)
+
+                    def __str__(self):
+                            return f"{self.address}, {self.city}, {self.state} {self.zip_code}"
