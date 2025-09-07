c5a8479bfe9c9d03db1b1aae9d503fcb24d11b6e
Bhagi7569 <bsarukolla@gmail.com>
2025-09-07 13:22:32 +0530

    42541

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+# models.py
+from django.db import models
 
-# Create your views here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+        address = models.CharField(max_length=200)
+
+        class OpeningHours(models.Model):
+            DAY_CHOICES = [
+                    ('Monday', 'Monday'),
+                            ('Tuesday', 'Tuesday'),
+                                    ('Wednesday', 'Wednesday'),
+                                            ('Thursday', 'Thursday'),
+                                                    ('Friday', 'Friday'),
+                                                            ('Saturday', 'Saturday'),
+                                                                    ('Sunday', 'Sunday'),
+                                                                        ]
+
+                                                                            restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+                                                                                day = models.CharField(max_length=10, choices=DAY_CHOICES)
+                                                                                    opening_time = models.TimeField()
+                                                                                        closing_time = models.TimeField()
+
+                                                                                            def __str__(self):
+                                                                                                    return f"{self.restaurant.name} - {self.day}"
