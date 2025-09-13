946a779d68e7a675ff493441ff87398187b3652e
Bhagi7569 <bsarukolla@gmail.com>
2025-09-13 16:49:55 +0530

    44401

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,9 @@
+# models.py
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+        phone_number = models.CharField(max_length=20)
+
+            def __str__(self):
+                    return self.name
