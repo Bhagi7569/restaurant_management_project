2d752e431acb262730398f942e31848c245eb409
Bhagi7569 <bsarukolla@gmail.com>
2025-09-15 17:22:00 +0530

    44397

--- home/models.py
+++ home/models.py
@@ -1,7 +1,19 @@
-# views.py
-from django.shortcuts import render
-from .models import Restaurant
+from django import forms
+from django.core.validators import EmailValidator
 
-def homepage(request):
-    restaurant = Restaurant.objects.first()  # Fetch the first restaurant
-        return render(request, 'homepage.html', {'restaurant': restaurant})
+class ContactForm(forms.Form):
+    email = forms.EmailField(
+            max_length=100,
+                    validators=[EmailValidator()],
+                            widget=forms.EmailInput(attrs={'placeholder': 'Your email'})
+                                )
+                                    message = forms.CharField(
+                                            widget=forms.Textarea(attrs={'placeholder': 'Your message'}),
+                                                    min_length=10
+                                                        )
+
+                                                            def clean_message(self):
+                                                                    message = self.cleaned_data['message']
+                                                                            if len(message) < 10:
+                                                                                        raise forms.ValidationError('Message must be at least 10 characters long.')
+                                                                                                return message
