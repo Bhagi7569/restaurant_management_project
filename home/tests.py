da50eb07f210380700ab6025b3dd39b64382e405
Bhagi7569 <bsarukolla@gmail.com>
2025-09-15 17:24:41 +0530

    44399

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.db import models
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+        history = models.TextField()
+            mission = models.TextField()
+
+                def __str__(self):
+                        return self.name
