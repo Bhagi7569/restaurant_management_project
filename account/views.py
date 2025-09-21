70c60f898972ffe418fea69f4cdf6fba7b71aca4
Bhagi7569 <bsarukolla@gmail.com>
2025-09-21 08:42:37 +0530

    47837

--- account/views.py
+++ account/views.py
@@ -4,4 +4,13 @@ class OrderStatus(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
         def __str__(self):
-                return self.name
+                return self.nameimport secrets
+                import string
+                from .models import Coupon  # Assuming you have a Coupon model
+
+                def generate_coupon_code(length=10):
+                    """Generate a unique alphanumeric coupon code."""
+                        while True:
+                                code = ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(length))
+                                        if not Coupon.objects.filter(code=code).exists():
+                                                    return code
