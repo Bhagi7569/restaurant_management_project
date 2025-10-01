e33e5361828fd9f450fcdc054ab18b00accfa1fc
Bhagi7569 <bsarukolla@gmail.com>
2025-10-01 10:37:32 +0530

    51322

--- products/apps.py
+++ products/apps.py
@@ -1,11 +1,11 @@
-# models.py
-from django.db import models
+# views.py
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
-class MenuItem(models.Model):
-    name = models.CharField(max_length=100)
-        description = models.TextField()
-            price = models.DecimalField(max_digits=6, decimal_places=2)
-                image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
-
-                    def __str__(self):
-                            return self.name
+class MenuItemList(generics.ListAPIView):
+    queryset = MenuItem.objects.all()
+        serializer_class = MenuItemSerializer
+            filter_backends = [DjangoFilterBackend]
+                filterset_fields = ['category__name']
