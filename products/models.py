9a1bd229d25141d25eddc87c72304265a1704bb5
Bhagi7569 <bsarukolla@gmail.com>
2025-10-01 10:39:38 +0530

    51320

--- products/models.py
+++ products/models.py
@@ -1,10 +1,16 @@
+# models.py
 from django.db import models
 
-# Create your models here.
-class Item(models.Model):
-    item_name = models.CharField(max_length=150)
-    item_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=255)
+        description = models.TextField()
+            price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    def __str__(self):
-        return str(self.item_name)
+            # serializers.py
+            from rest_framework import serializers
+            from .models import MenuItem
+
+            class MenuItemSerializer(serializers.ModelSerializer):
+                class Meta:
+                        model = MenuItem
+                                fields = ['id',
